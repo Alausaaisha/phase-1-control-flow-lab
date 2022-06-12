ce67f641c194d0923c01cd55e162ab8d41b4d835
Alausa Aishat <qtaisha.ao@gmail.com>
2022-06-12 23:36:58 +0100

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let ride;
+  if (distance <= 400){
+    ride = 'This one is on me!';
+  }
+  if (distance > 2000){
+    ride = 'I will gladly take your thirty bucks.';
+  }
+  if (distance > 2500){
+    ride = 'No can do.';
+  }
+  return (ride);
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+ // let ride;
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
